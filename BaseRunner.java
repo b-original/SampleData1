0c55b66bfa1828c9cb790404246d7d7fb08c2822
Bhavya Varshney <guptabhavya0@gmail.com>
2023-01-12 09:28:26 +0530

    updated with some variable accessor checks

--- BaseRunner.java
+++ BaseRunner.java
@@ -6,9 +6,20 @@ public class BaseRunner{
 		System.out.println("File running");
 		
 		testCaller testC= new testCaller();
+		//to check if  private variable is accessiable from main class
+		//testC.var= 15;
+		/*got below Error for private variable 
+		BaseRunner.java:10: error: var has private access in testCaller
+                testC.var= 15;
+		*/
+		//to check if default variable is accessiable from main class
+		testC.var2=90;
+		//success
+		
 		testCaller testC2= new testCaller(5);
 		
 		System.out.println("var value "+testC.getVar());
+		System.out.println("var value "+testC.var2);
 		System.out.println("var2 value "+testC2.getVar());
 		System.out.println("File End");
 
@@ -21,6 +32,7 @@ class testCaller{
 	
 	private int var;
 	
+	int var2;
 	public testCaller(){
 		var=10;
 	}
